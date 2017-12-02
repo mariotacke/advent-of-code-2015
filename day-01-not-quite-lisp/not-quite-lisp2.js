d612a55ebcc1965d5c141e283cc43845dd5ec56b
Mario Tacke <tacke.mario@gmail.com>
2017-12-01 20:57:09 -0800

    feature: Not Quite Lisp (part 2)

---
+++ day-01-not-quite-lisp/not-quite-lisp2.js
@@ -0,0 +1,15 @@
+const notQuiteLisp = (input) => {
+  const array = input.split('');
+
+  let currentFloor = 0;
+  
+  for (let i = 0; i < array.length; i++) {
+    currentFloor += array[i] === '(' ? 1 : -1;
+    
+    if (currentFloor === -1) {
+      return i + 1;
+    }
+  }
+};
+
+module.exports = notQuiteLisp;
--- day-01-not-quite-lisp/test.js
+++ day-01-not-quite-lisp/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 
 const notQuiteLisp = require('./not-quite-lisp');
+const notQuiteLisp2 = require('./not-quite-lisp2');
 
 describe('Day 1: Not Quite Lisp', () => {
   it('should properly calculate (()) and ()()', () => {
@@ -25,5 +26,15 @@ describe('Day 1: Not Quite Lisp', () => {
   it('should properly calculate ))) and )())())', () => {    
     assert.equal(-3, notQuiteLisp(')))'));
     assert.equal(-3, notQuiteLisp(')())())'));
+  });
+  
+  describe('Part 2', () => {
+    it('should properly calculate )', () => {
+      assert.equal(1, notQuiteLisp2(')'));
+    });
+    
+    it('should properly calculate ()())', () => {
+      assert.equal(5, notQuiteLisp2('()())'));
+    });
   });
 });
