ec36f1c63ac6150258964473d6b1b64cf3f00091
Mario Tacke <tacke.mario@gmail.com>
2017-12-05 21:30:33 -0800

    feature: I Was Told There Would Be No Math (part 2)

---
+++ day-02-i-was-told-there-would-be-no-math/ribbon.js
@@ -0,0 +1,29 @@
+const ribbon = (input) => {
+  return input
+    .split('\n')
+    .map((x) => {
+      const measurements = x.trim().split('x');
+      
+      return {
+        length: measurements[0],
+        width: measurements[1],
+        height: measurements[2],
+      }
+    })
+    .reduce((accumulator, currentValue) => {
+      const { length, width, height } = currentValue;
+      const frontBack = 2 * width + 2 * height;
+      const topBottom = 2 * width + 2 * length;
+      const sides = 2 * height + 2 * length;
+      
+      const smallestPerimeter = Math.min(
+        frontBack,
+        topBottom,
+        sides,
+      );
+      
+      return accumulator + smallestPerimeter + length * width * height;
+    }, 0);
+};
+
+module.exports = ribbon;
--- day-02-i-was-told-there-would-be-no-math/test.js
+++ day-02-i-was-told-there-would-be-no-math/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 
 const wrappingPaper = require('./wrapping-paper');
+const ribbon = require('./ribbon');
 
 describe('Day 2: I Was Told There Would Be No Math', () => {
   it('should properly calculate 2x3x4', () => {    
@@ -9,5 +10,15 @@ describe('Day 2: I Was Told There Would Be No Math', () => {
   
   it('should properly calculate 1x1x10', () => {
     assert.equal(43, wrappingPaper('1x1x10'));
+  });
+  
+  describe('Part Two', () => {
+    it('should properly calculate 2x3x4', () => {    
+      assert.equal(34, ribbon('2x3x4'));
+    });
+    
+    it('should properly calculate 1x1x10', () => {
+      assert.equal(14, ribbon('1x1x10'));
+    });
   });
 });
