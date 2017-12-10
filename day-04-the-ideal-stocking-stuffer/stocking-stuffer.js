7eaae95c59b14c5d60bcb13d44ecd913555f7d56
Mario Tacke <tacke.mario@gmail.com>
2017-12-10 14:54:52 -0800

    feature: The Ideal Stocking Stuffer (part 2)

--- day-04-the-ideal-stocking-stuffer/stocking-stuffer.js
+++ day-04-the-ideal-stocking-stuffer/stocking-stuffer.js
@@ -1,13 +1,14 @@
 const md5 = require('md5');
 
-const stockingStuffer = (input) => {
+const stockingStuffer = (input, leadingZeroes = 5) => {
+  const zeroes = '0'.repeat(leadingZeroes);
   let iteration = 1;
 
   // eslint-disable-next-line no-constant-condition
   while (true) {
     const hash = md5(`${input}${iteration++}`);
 
-    if (hash.substring(0, 5) === '00000') {
+    if (hash.substring(0, leadingZeroes) === zeroes) {
       return iteration - 1;
     }
   }
