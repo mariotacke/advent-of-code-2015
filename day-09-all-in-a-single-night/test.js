833f4f4e06f0e7e82deb5c20a15e400c0dd7ecee
Mario Tacke <tacke.mario@gmail.com>
2018-11-05 21:07:16 -0800

    feature: All in a Single Night (part 2)

--- day-09-all-in-a-single-night/test.js
+++ day-09-all-in-a-single-night/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 
 const distance = require('./shortest-distance');
+const distance2 = require('./shortest-distance2');
 
 describe('Day 9: All in a Single Night', () => {
   it('should calculate shortest distance', () => {
@@ -12,5 +13,18 @@ describe('Day 9: All in a Single Night', () => {
     const shortest = distance(distances);
 
     assert.strictEqual(shortest, 605);
+  });
+
+  describe('Part Two', () => {
+    it('should calculate longest distance', () => {
+      const distances =
+        `London to Dublin = 464
+         London to Belfast = 518
+         Dublin to Belfast = 141`;
+
+      const shortest = distance2(distances);
+
+      assert.strictEqual(shortest, 982);
+    });
   });
 });
