e730e81088820fd8c36077b2471a58ff27e862cb
Mario Tacke <tacke.mario@gmail.com>
2018-11-14 15:43:50 -0800

    feature: Like a GIF For Your Yard (part 2)

--- day-18-like-a-gif-for-your-yard/test.js
+++ day-18-like-a-gif-for-your-yard/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 
 const gif = require('./gif');
+const gif2 = require('./gif2');
 
 describe('Day 18: Like a GIF For Your Yard', () => {
   it('should calculate final state lights', () => {
@@ -14,5 +15,20 @@ describe('Day 18: Like a GIF For Your Yard', () => {
     const steps = 4;
 
     assert.strictEqual(gif(grid, steps), 4);
+  });
+
+  describe('Part Two', () => {
+    it('should consider stuck lights', () => {
+      const grid =
+        `##.#.#
+         ...##.
+         #....#
+         ..#...
+         #.#..#
+         ####.#`;
+      const steps = 5;
+
+      assert.strictEqual(gif2(grid, steps), 17);
+    });
   });
 });
