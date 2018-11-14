3b7ed0aeea13fa6ad35335c0238b2b87af8f955a
Mario Tacke <tacke.mario@gmail.com>
2018-11-13 20:40:01 -0800

    feature: No Such Thing as Too Much (part 2)

---
+++ day-17-not-such-thing-as-too-much/fluids2.js
@@ -0,0 +1,40 @@
+const getCombinations = (volume, containers) => {
+  const combinations = [];
+
+  const combine = (array, carry = []) => {
+    for (let i = 0; i < array.length; i++) {
+      const root = array[i];
+      const nodes = [...array.slice(i, i), ...array.slice(i + 1)]
+      const nextCarry = [...carry, root];
+
+      if (nextCarry.reduce((a, b) => a + b, 0) === volume) {
+        combinations.push(nextCarry);
+      }
+
+      combine(nodes, nextCarry);
+    }
+  };
+
+  combine(containers, []);
+
+  return combinations;
+};
+
+const fluids = (volume, containers) => {
+  containers = containers
+    .split('\n')
+    .map((x) => parseInt(x))
+    .sort((a, b) => b - a);
+
+  const combinationLengths = getCombinations(volume, containers)
+    .map((combination) => combination.length);
+
+  const minimum = Math.min(...combinationLengths);
+  const minimumCombinations = combinationLengths
+    .filter((length) => length === minimum)
+    .length;
+
+  return minimumCombinations;
+};
+
+module.exports = fluids;
--- day-17-not-such-thing-as-too-much/test.js
+++ day-17-not-such-thing-as-too-much/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 
 const fluids = require('./fluids');
+const fluids2 = require('./fluids2');
 
 describe('Day 17: No Such Thing as Too Much', () => {
   it('should calculate possible combinations', () => {
@@ -12,5 +13,18 @@ describe('Day 17: No Such Thing as Too Much', () => {
        5`;
 
     assert.strictEqual(fluids(25, containers), 4);
+  });
+
+  describe('Part Two', () => {
+    it('should calculate minimum possible combinations', () => {
+      const containers =
+        `20
+         15
+         10
+         5
+         5`;
+
+      assert.strictEqual(fluids2(25, containers), 3);
+    });
   });
 });
