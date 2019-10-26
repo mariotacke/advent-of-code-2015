7fb2092035c447e38b327b9525b1049f1a73abb9
Mario Tacke <tacke.mario@gmail.com>
2019-10-25 22:11:31 -0700

    feat: Day 21: RPG Simulator 20XX (part 2)

--- day-21-rpg-simulator-20xx/test.js
+++ day-21-rpg-simulator-20xx/test.js
@@ -1,6 +1,7 @@
 const assert = require('assert');
 
 const rpg = require('./rpg');
+const rpg2 = require('./rpg2');
 
 describe('Day 21: RPG Simulator 20XX', () => {
   it('should simulate fight', () => {
@@ -15,5 +16,21 @@ describe('Day 21: RPG Simulator 20XX', () => {
     const minimumGoldSpentToWin = rpg(bossStats, playerStats);
 
     assert.deepEqual(minimumGoldSpentToWin, 65);
+  });
+
+  describe('Part Two', () => {
+    it('should simulate rigged fight', () => {
+      const bossStats = {
+        hp: 12,
+        damage: 7,
+        armor: 2,
+      };
+
+      const playerStats = { hp: 8 };
+
+      const maximumGoldSpentAndStillLose = rpg2(bossStats, playerStats);
+
+      assert.deepEqual(maximumGoldSpentAndStillLose, 188);
+    });
   });
 });
